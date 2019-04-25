c6e2b2b7b0cfac591cf4da6f35a6db223064088b
Daniel Bridgewater <danielbridgewater87@gmail.com>
2019-04-25 22:52:59 +0100

    Add project files.

---
+++ Pandora/Kernel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Sys = Cosmos.System;
+
+namespace Pandora
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+        }
+
+        protected override void Run()
+        {
+            for (; ; )
+            {
+                Console.Write("Enter text: ");
+                string input = Console.ReadLine();
+                Console.WriteLine("You entered: "+input);
+            }
+        }
+    }
+}
