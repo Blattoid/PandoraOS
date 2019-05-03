0e59808d094f527b6a0233ce6e05745e5a68f6f6
Daniel Bridgewater <danielbridgewater87@gmail.com>
2019-05-03 20:16:56 +0100

    Switch-case statements do not support variable scoping, which is a pain to deal with. Reverts back to if/else chains

--- Pandora/Kernel.cs
+++ Pandora/Kernel.cs
@@ -6,8 +6,9 @@ namespace Pandora
 {
     public class Kernel : Sys.Kernel
     {
-        public double SYS_VERSION = 0.3;
+        public double SYS_VERSION = 0.31;
         MissingFunctions functions = new MissingFunctions();
+        public Sys.FileSystem.CosmosVFS filesys;
 
         protected override void BeforeRun()
         {
@@ -22,6 +23,7 @@ namespace Pandora
 
         protected override void Run()
         {
+            Console.WriteLine(Console.ReadLine().Length+" characters");
             try
             {
                 //read user command
@@ -29,11 +31,10 @@ namespace Pandora
                 string[] input = Console.ReadLine().Split(" "); //split by spaces
                 string command = input[0].ToLower(); //grab lowercase of command
 
-                switch (command)
+                if (command == "help")
                 {
-                    case "help":
-                        foreach (string line in new string[]
-                            {
+                    foreach (string line in new string[]
+                        {
                                 "help",
                                 "",
                                 "init_vfs",
@@ -42,88 +43,85 @@ namespace Pandora
                                 "",
                                 "reboot",
                                 "shutdown"
-                            }
-                        ) Console.WriteLine(line);
-                        break;
-                    case "init_vfs":
-                        var fs = new Sys.FileSystem.CosmosVFS();
-                        Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
-                        Success("Initialised VFS.");
-                        break;
-                    case "list":
-                        string cd = Directory.GetCurrentDirectory();
-                        Console.WriteLine("Directory listing for " + cd);
-
-                        ScrollWithPauses(Directory.GetDirectories(cd), "<DIR>  ", ConsoleColor.Magenta);
-                        ScrollWithPauses(Directory.GetFiles(cd), "<FILE> ", ConsoleColor.Green);
-                        Console.ResetColor();
-                        break;
-                    case "memopad":
-                        Console.WriteLine("ALT+C to exit.");
-                        ConsoleKeyInfo key; //key pressed
-
-                        for (; ; )
-                        {
-                            int x = Console.CursorLeft;
-                            int y = Console.CursorTop;
-                            key = Console.ReadKey(true);
-
-                            //update modifier states
-                            bool ALT = false;
-                            bool SHIFT = false;
-                            if ((key.Modifiers & ConsoleModifiers.Alt) != 0) ALT = true;
-                            if ((key.Modifiers & ConsoleModifiers.Shift) != 0) SHIFT = true;
-
-                            if (ALT && key.KeyChar == 'c') break; //exit program
-                            //backspace implementation
-                            else if (key.Key == ConsoleKey.Backspace)
-                            {
-                                Console.CursorLeft--;
-                                Console.Write(" ");
-                                x--;
-                            }
-                            else if (key.Key == ConsoleKey.Enter)
-                            {
-                                x = 0;
-                                y++;
-                            }
-                            //cursor key handling
-                            else if (key.Key == ConsoleKey.RightArrow) x++;
-                            else if (key.Key == ConsoleKey.LeftArrow) x--;
-                            else if (key.Key == ConsoleKey.DownArrow) y++;
-                            else if (key.Key == ConsoleKey.UpArrow) y--;
-
-                            else
-                            {
-                                Console.Write(key.KeyChar); //if the key pressed was a normal character, print it.
-                                x++;
-                            }
-
-                            //scroll if y exceeds screen height
-                            if (y > Console.WindowHeight)
-                            {
-                                y = Console.WindowHeight - 3;
-                                Console.WriteLine();
-                            }
-                            else if (y < 0) y = 0;
-                            else
-                            {
-                                Console.SetCursorPosition(x, y); //update cursor position
-                            }
-
                         }
-                        Console.WriteLine();
-                        break;
-                    case "reboot":
-                        Sys.Power.Reboot();
-                        break;
-                    case "shutdown":
-                        Sys.Power.Shutdown();
-                        break;
-                    default:
-                        Error("Unknown command.");
-                        break;
+                    ) Console.WriteLine(line);
                 }
+                else if (command == "init_vfs")
+                {
+                    filesys = new Sys.FileSystem.CosmosVFS();
+                    Sys.FileSystem.VFS.VFSManager.RegisterVFS(filesys);
+                    Success("Initialised VFS.");
+                }
+                else if (command == "list")
+                {
+                    string cd = Directory.GetCurrentDirectory();
+                    Console.WriteLine("Directory listing for " + cd);
+
+                    ScrollWithPauses(Directory.GetDirectories(cd), "<DIR>  ", ConsoleColor.Magenta);
+                    ScrollWithPauses(Directory.GetFiles(cd), "<FILE> ", ConsoleColor.Green);
+                    Console.ResetColor();
+                }
+                else if (command == "memopad")
+                {
+                    ConsoleKeyInfo key; //key pressed
+
+                    Console.WriteLine("Press ALT+C to exit.");
+                    for (; ; )
+                    {
+                        int x = Console.CursorLeft;
+                        int y = Console.CursorTop;
+                        key = Console.ReadKey(true);
+
+                        //update modifier states
+                        bool ALT = false;
+                        bool SHIFT = false;
+                        if ((key.Modifiers & ConsoleModifiers.Alt) != 0) ALT = true;
+                        if ((key.Modifiers & ConsoleModifiers.Shift) != 0) SHIFT = true;
+
+                        if (ALT && key.KeyChar == 'c') break; //exit program
+                                                              //backspace implementation
+                        else if (key.Key == ConsoleKey.Backspace)
+                        {
+                            Console.CursorLeft--;
+                            Console.Write(" ");
+                            x--;
+                        }
+                        else if (key.Key == ConsoleKey.Enter)
+                        {
+                            x = 0;
+                            y++;
+                        }
+                        //cursor key handling
+                        else if (key.Key == ConsoleKey.RightArrow) x++;
+                        else if (key.Key == ConsoleKey.LeftArrow) x--;
+                        else if (key.Key == ConsoleKey.DownArrow) y++;
+                        else if (key.Key == ConsoleKey.UpArrow) y--;
+
+                        else
+                        {
+                            Console.Write(key.KeyChar); //if the key pressed was a normal character, print it.
+                            x++;
+                        }
+
+                        Console.SetCursorPosition(0, 0);
+                        Console.Write(x + ", " + y + " ");
+
+                        //loop cursor if it goes past the screen edge
+                        if (y > Console.WindowHeight - 1) y = 0;
+                        if (x < 0)
+                        {
+                            //cursor should go to the end of the previous line
+                            x = Console.WindowWidth;
+                            y--;
+                        }
+                        else if (y < 0) y = 0;
+                        Console.SetCursorPosition(x, y); //update cursor position
+                    }
+                    Console.WriteLine();
+                }
+                else if (command == "reboot") Sys.Power.Reboot();
+                else if (command == "shutdown") Sys.Power.Shutdown();
+                else Error("Unknown command.");
             }
             catch (Exception err) { Error(err.Message); }
         }
@@ -146,7 +144,8 @@ namespace Pandora
         void ScrollWithPauses(string[] scrolltext, string messageprefix = "", ConsoleColor textcolour = ConsoleColor.White)
         {
             int line = 0;
-            foreach (string entry in scrolltext) {
+            foreach (string entry in scrolltext)
+            {
                 Console.ForegroundColor = textcolour;
                 Console.WriteLine(messageprefix + entry);
 
