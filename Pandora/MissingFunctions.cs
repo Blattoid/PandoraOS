068d17f6ea62c67495002e274e04f03b7661022a
Daniel Bridgewater <danielbridgewater87@gmail.com>
2019-05-08 20:48:11 +0100

    Replaces the old and ugly ConvertToInt function with a new and prettier one that can also handle negative numbers.

--- Pandora/MissingFunctions.cs
+++ Pandora/MissingFunctions.cs
@@ -1,57 +1,57 @@
 ﻿using System;
+using System.Text;
 
 namespace Pandora
 {
     //I've noticed that some things have not been implemented in Cosmos that I need, so these are some workarounds for those problems.
     class MissingFunctions
     {
-        //Convert.ToInt32 doesn't work, so I have to implement it myself in such a way that it does work. It will not be pretty.
+        //Convert.ToInt32 doesn't work, so I have to implement it myself.
         public int ConvertToInt(string input)
         {
-            Int32 output = 0;
+            var throwerror = new Action(() => { throw new FormatException("Input string was not in the correct format."); });
+
             int multiplier = 1;
-            for (int i = input.Length - 1; i >= 0; i -= 1)
+            int result = 0;
+            bool is_negative = false; //is the number negative?
+
+            Console.Write(":");
+            byte[] data = Encoding.ASCII.GetBytes(Console.ReadLine());
+            for (int i = data.Length - 1; i >= 0; i--) //loop over the digits in reverse so we process the 1's digit first.
             {
-                int x = 0;
-                string y = input[i].ToString();
-                switch (y)
+                int code = (int)data[i]; //convert the byte to an integer.
+
+                if (code == 45) //hyphon check
                 {
-                    case "0":
-                        x = 0;
-                        break;
-                    case "1":
-                        x = 1;
-                        break;
-                    case "2":
-                        x = 2;
-                        break;
-                    case "3":
-                        x = 3;
-                        break;
-                    case "4":
-                        x = 4;
-                        break;
-                    case "5":
-                        x = 5;
-                        break;
-                    case "6":
-                        x = 6;
-                        break;
-                    case "7":
-                        x = 7;
-                        break;
-                    case "8":
-                        x = 8;
-                        break;
-                    case "9":
-                        x = 9;
-                        break;
+                    if (i == 0) //if the input starts with a hyphon, the number is negative.
+                    {
+                        is_negative = true; //set the flag.
+                    }
+                    //but if the hyphon appears later in the string, the string is malformed.
+                    else throwerror();
                 }
-                output += x * multiplier;
-                multiplier *= 10;
+
+                //matches if the character is not between 0 and 9 and isn't a hyphon.
+                // 48 is decimal in ascii for 0, and similar is true for 57 meaning 9.
+                else if (!(code >= 48 && code <= 57)) throwerror();  //character code exceeds range - throw an error.
+
+                if (is_negative)
+                {
+                    result -= (result * 2); //make the result a negative integer by subtracting itself twice.
+                }
+                else
+                {
+                    //the code has been validated to be a digit - convert it to an integer.
+                    //the conversion from ascii decimal --> integer is to just subtract 48.
+                    code -= 48;
+                    //then we multiply it by our multiplier to it is the correct place.
+                    code *= multiplier;
+                    //finally, add it to the result.
+                    result += code;
+                }
+                multiplier *= 10; //multiply the multiplier by 10 since we are about to process the next digit.
             }
-            return output;
-            
+            return result; //done!
         }
 
         //Because System.Linq isn't supported, Enumerable.Repeat also doesn't exist.
